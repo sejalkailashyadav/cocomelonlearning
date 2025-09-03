361d4486a3c0e64a2dcf2ce1c610642e5fb423de
sejal yadav <sejalyadav0818@gmail.com>
2025-09-03 11:43:04 +0530

    chore:added heading

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <!-- PAGE-HEADER -->
 <div class="page-header">
-    <h1 class="page-title">Cocomelonlearning Magenta Dashboard</h1>
+    <h1 class="page-title"> Main  : Cocomelonlearning Magenta Dashboard</h1>
     <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="javascript:void(0);"></a></li>
         <li class="breadcrumb-item active" aria-current="page"> </li>
